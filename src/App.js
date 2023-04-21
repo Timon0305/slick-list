8e31a20d504ceb418bce30a7c5444b1ed36a3c7f
Timon0305 <timon0305@outlook.com>
2023-04-21 10:55:38 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
 
-function App() {
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slider {...settings}>
+      <div className="text-center">
+        <h3>1</h3>
+      </div>
+      <div className="text-center">
+        <h3>2</h3>
+      </div>
+      <div className="text-center">
+        <h3>3</h3>
+      </div>
+      <div className="text-center">
+        <h3>4</h3>
+      </div>
+      <div className="text-center">
+        <h3>5</h3>
+      </div>
+      <div className="text-center">
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
 }
-
-export default App;
